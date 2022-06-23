d04557937e84d6cc93b7134baf412ace50b53f59
KamalKumar <kamalkumar77a@gmail.com>
2022-06-23 14:51:23 +0500

    commit

--- src/main/java/com/example/HibernateAdvanced/Controllers/ProductController.java
+++ src/main/java/com/example/HibernateAdvanced/Controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.example.HibernateAdvanced.Controllers;
 import com.example.HibernateAdvanced.Pojos.AddProduct;
 import com.example.HibernateAdvanced.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +18,10 @@ public class ProductController {
     @PostMapping("/")
     public String addProduct(@RequestBody AddProduct addProduct) throws Exception{
         return productService.AddProduct(addProduct);
+    }
+
+    @GetMapping("/getproduct")
+    public void getProduct(){
+
     }
 }
